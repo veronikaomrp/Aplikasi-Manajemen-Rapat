1bb3d1a4e8366634485f3b5ed12843f071c7bad0
Fedrick Siagian <superiorsiagian@gmail.com>
2021-01-17 20:21:01 +0700

    3 functioni

---
+++ register.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once("config.php");
+
+if(isset($_POST['register'])){
+
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+
+    $sql = "INSERT INTO users (name, username, email, password) 
+            VALUES (:name, :username, :email, :password)";
+    $stmt = $db->prepare($sql);
+
+    $params = array(
+        ":name" => $name,
+        ":username" => $username,
+        ":password" => $password,
+        ":email" => $email
+    );
+
+    $saved = $stmt->execute($params);
+
+    if($saved) header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+        <a href="index.php">Login di sini</a></p>
+
+        <form action="" method="POST">
+
+            <div class="form-group">
+                <label for="name">Nama</label>
+                <input class="form-control" type="text" name="name" placeholder="Nama kamu" />
+            </div>
+
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input class="form-control" type="text" name="username" placeholder="Username" />
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" type="email" name="email" placeholder="Alamat Email" />
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password" placeholder="Password" />
+            </div>
+
+            <input type="submit" class="btn btn-success btn-block" name="register" value="Daftar" />
+
+        </form>
+            
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
